b4b331ebe133b99f3df54fcb930dcdfb43e58581
Julien <julien.alexandre26@gmail.com>
2018-04-26 09:02:54 +0200

    Création de la méthode indexAction de ListingController. Création de la vue listing.

--- src/AppBundle/Controller/ListingController.php
+++ src/AppBundle/Controller/ListingController.php
@@ -11,7 +11,31 @@ use AppBundle\Entity\Flight;
 use AppBundle\Entity\PlaneModel;
 use AppBundle\Entity\Reservation;
 
+/**
+ * Class ListingController
+ * @Route(*listing*)
+ */
 class ListingController extends Controller
 {
-
+    /**
+     * List one reservation with one flight and one planemodel, with few IDs.
+     *
+     * @Route("/{reservation_id}/flight/{flight_id}/planemodel/{planemodel_id}", name="listing_index", requirements={"reservation_id": "\d+"})
+     * @Method("GET)
+     * @ParamConverter("reservation", options={"mapping": {"reservation_id": "id"}})
+     * @ParamConverter("flight", options={"mapping": {"flight_id": "id"}})
+     * @ParamConverter("planemodel", options={"mapping": {"planemodel_id": "id"}})
+     * @param Reservation $reservation
+     * @param Flight $flight
+     * @param PlaneModel $planemodel
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Reservation $reservation, Flight $flight, PlaneModel $planemodel)
+    {
+        return $this->render('listing/index.html.twig', array(
+            'reservation' => $reservation,
+            'flight' => $flight,
+            'planemodel' => $planemodel
+        ));
+    }
 }
