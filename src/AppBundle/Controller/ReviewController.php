190a1445ef5c8c472a076af244a391563494f355
Julien <julien.alexandre26@gmail.com>
2018-05-02 14:05:45 +0200

    Création du formulaire de création de review.

--- src/AppBundle/Controller/ReviewController.php
+++ src/AppBundle/Controller/ReviewController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Review;
+use AppBundle\Form\ReviewType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -42,9 +43,20 @@ class ReviewController extends Controller
     public function newAction(Request $request)
     {
         $review = new Review();
+        $form = $this->createForm(ReviewType::class, $review);
+
+        $form->$this->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($review);
+            $em->flush();
+            return $this->redirectToRoute('review_show', array('id' => $review->getId()));
+        }
 
         return $this->render('review/new.html.twig', array(
             'review' => $review,
+            'form' => $form->createView(),
         ));
     }
 }
